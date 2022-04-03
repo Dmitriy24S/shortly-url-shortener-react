ab8665965b92243cd6ba89b2096260a5d90ce2c7
Dmitriy <devilins24@gmail.com>
2022-04-04 00:15:55 +0300

    add desktop footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,17 @@ import { ReactComponent as IGLogo } from "../images/icon-instagram.svg";
 export const Footer = () => {
   return (
     <footer className="text-white">
-      <section className="footer-top px-6 py-24 flex flex-col gap-6 items-center">
-        <h2 className="font-bold text-3xl text-white">Boost your links today</h2>
+      <section className="footer-top px-6 py-24 flex flex-col gap-6 items-center md:gap-8">
+        <h2 className="font-bold text-3xl text-white md:text-5xl">Boost your links today</h2>
         <button href="#/" className="sign-up-btn block py-3 px-12 rounded-full">
           Get Started
         </button>
       </section>
 
-      <section className="footer-bottom py-16 flex flex-col gap-12">
-        <Logo className="footer-logo mx-auto" />
-
-        <div className="footer-links flex flex-col  gap-12 ">
-          <div className="footer-links-section flex flex-col gap-5">
+      <section className="footer-bottom py-16 flex flex-col gap-12 md:flex-row md:justify-evenly md:px-4">
+        <Logo className="footer-logo mx-auto md:m-0" />
+        <div className="footer-links flex flex-col  gap-12 md:flex-row xl:gap-24">
+          <div className="footer-links-section flex flex-col gap-5 md:text-left ">
             <h4 className="font-semibold text-white">Features</h4>
             <ul className="flex flex-col gap-2 text-zinc-400">
               <li>
@@ -34,7 +33,7 @@ export const Footer = () => {
             </ul>
           </div>
 
-          <div className="footer-links-section flex flex-col gap-5">
+          <div className="footer-links-section flex flex-col gap-5 md:text-left">
             <h4 className="font-semibold text-white">Resources</h4>
             <ul className="flex flex-col gap-2 text-zinc-400">
               <li>
@@ -49,7 +48,7 @@ export const Footer = () => {
             </ul>
           </div>
 
-          <div className="footer-links-section flex flex-col gap-5">
+          <div className="footer-links-section flex flex-col gap-5 md:text-left">
             <h4 className="font-semibold text-white">Company</h4>
             <ul className="flex flex-col gap-2 text-zinc-400">
               <li>
@@ -69,7 +68,7 @@ export const Footer = () => {
         </div>
 
         <div className="social-links">
-          <ul className="flex gap-6 justify-center">
+          <ul className="flex gap-6 justify-center items-center">
             <li>
               <a href="#/">
                 <FBLogo />
